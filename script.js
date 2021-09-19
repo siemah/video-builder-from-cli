d7e5e8ee12ccdd48833b2f05e33fcf6edd2d22d6
Ubuntu <ubuntu@ip-172-31-17-189.eu-west-3.compute.internal>
2021-09-19 12:16:22 +0000

    wav2mp3 & create video

---
+++ script.js
@@ -0,0 +1,64 @@
+var ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
+var ffmpeg = require('fluent-ffmpeg');
+var videoshow = require('videoshow')
+
+let _timemark;
+
+(async () => {
+  console.log('convert wav to mp3')
+  const _time = Date.now();
+  const audioPath = `converter-${_time}.mp3`;
+  const imagePath = __dirname + '/assets/bg.jpeg';
+  const videoPath = `${__dirname}/videoshow-v1-${_time}.mp4`;
+
+  try {
+    await wav2mp3(`${__dirname}/assets/untitled.wav`, audioPath);
+    console.log('create video')
+    const res = await createVideo(imagePath, audioPath, videoPath);
+    console.log('output:', res)
+  } catch (error) {
+    console.log(error);
+  }
+})()
+// todo: upload created video 2youtube
+
+
+function wav2mp3(sourcePath, distinationPath) {
+  return new Promise((resolve, reject) => {
+    ffmpeg(sourcePath)
+      .inputFormat('wav')
+      .audioBitrate(192)
+      .format('mp3')
+      .save(distinationPath)
+      .on('end', resolve)
+      .on('error', reject)
+      .run();
+  });
+}
+
+function createVideo(imagePath, audioPath, videoPath) {
+  return new Promise((resolve, reject) => {
+    var images = [imagePath];
+
+    videoshow(images)
+      .audio(audioPath)
+      .save(videoPath)
+      .on('start', function (command) {
+        console.log('ffmpeg process started:', command)
+      })
+      .on('error', function (err) {
+        console.error('Error:', err)
+        reject(err);
+      })
+      .on('progress', function onProgress(progress) {
+        if (progress.timemark != _timemark) {
+          _timemark = progress.timemark;
+          console.log('Time mark: ' + _timemark + "...");
+        }
+      })
+      .on('end', function (output) {
+        console.log('Video created in:', output)
+        resolve(output)
+      })
+  });
+}
