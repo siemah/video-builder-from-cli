52473b093e225ac6f2e2607b1e26c926bf08b1b6
siemah <hmi.dayenio@gmail.com>
2021-09-19 16:37:11 +0000

    use examples from repos videoshow

---
+++ example.js
@@ -0,0 +1,15 @@
+const videoshow = require('videoshow')
+const images = [`${__dirname}/assets/bg.jpeg`]
+
+videoshow(images)
+  .audio(`${__dirname}/converter.mp3`, {fade:true})
+  .save('example-video.mp4')
+  .on('start', function (command) {
+    console.log('ffmpeg process started:', command)
+  })
+  .on('error', function (err) {
+    console.error('Error:', err)
+  })
+  .on('end', function (output) {
+    console.log('Video created in:', output)
+  })
